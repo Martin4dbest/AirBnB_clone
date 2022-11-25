a5c6aaa93b160595b33e3ef4e8bec7845901a373
Martin4dbest <martin4dtruth4dtruth@gmail.com>
2022-11-24 20:25:58 -0800

    Update models

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,7 @@ from models import storage
 
 class BaseModel:
 
-    """Class from which all other classes will inherit"""
+    """This is Class from which all other classes will inherit"""
 
     def __init__(self, *args, **kwargs):
         """Initializes instance attributes
